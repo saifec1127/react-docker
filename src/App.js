2651c33e7b498ec132f08aa0a031ec4ed2b817d9
saif khan <saif@LAPTOP-7JT8713E>
2024-06-13 14:00:09 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
          react project with Docker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+         running with docker image
+        </p>
       </header>
     </div>
   );
